9157be78e92503ed0b6dae97e488cb3633b9bde5
kleberandradejua <kleberandrade98@gmail.com>
2024-09-20 17:15:35 -0300

    teste

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,19 @@
 /// <reference types='cypress'/>
 
 describe('login', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('https://barrigareact.wcaquino.me/')    
   })
   
-  it('login no sistema', () => {
+  it('Acesso ao sistema', () => {
     cy.get('[data-test="email"]').type('kleberandrade98@gmail.com')
     cy.get('[data-test="passwd"]').type('Ble@ch2206')
     cy.get('.btn').click()
-    
+    cy.get('.toast-message').contains('Bem vindo, Kleber de Andrade!')    
+  })
+  it('Erro de senha', () => {
+    cy.get('[data-test="email"]').type('kleberandrade98@gmail.com')
+    cy.get('[data-test="passwd"]').type('Blech2206')
+    cy.get('.btn').click()
   })
 })
