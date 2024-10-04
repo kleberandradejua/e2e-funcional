7ca9b2e926b90897ab76ce84c22fe898d9bd98fd
kleber-moura-brisa <kleber.moura@grupobrisanet.com.br>
2024-10-04 17:08:58 -0300

    Fluxo cadastro conta

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -16,4 +16,28 @@ describe('login', () => {
     cy.get('[data-test="passwd"]').type('Blech2206')
     cy.get('.btn').click()
   })
+  it('Conta errada', () => {
+    cy.get('[data-test="email"]').type('kleberandrade98@gmail.co')
+    cy.get('[data-test="passwd"]').type('Ble@ch2206')
+    cy.get('.btn').click()
+  })
+  it.only('Inserir conta', () => {
+    cy.get('[data-test="email"]').type('kleberandrade98@gmail.com')
+    cy.get('[data-test="passwd"]').type('Ble@ch2206')
+    cy.get('.btn').click()
+    cy.get('.toast-message').contains('Bem vindo, Kleber de Andrade!')
+    cy.get('[data-test="menu-settings"]').click()
+    cy.get('[href="/contas"]').click()
+    cy.get('[data-test="nome"]')
+    .click()
+    .type('Antônio Vinicius')
+    cy.get('.btn').click()
+    cy.get('.toast-success').should('contain.text','Conta inserida com sucesso!')
+    cy.get('tbody > tr > :nth-child(1)').should('have.text', 'Antônio Vinicius')
+
+
+
+
+
+  })
 })
